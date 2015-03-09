5b72771692d91aafcccbca1f7cd495d9120af05b
shotaroikeda <ikeda.shot@gmail.com>
2015-03-09 00:14:17 -0500

    Finished mysqldb module

--- backend/bin/main.py
+++ backend/bin/main.py
@@ -1,5 +1,26 @@
 from module.opendatabase import *
 
-if __name__ == '__main__':
+
+def testcase():
     db = ManageTable('localhost', 'testuser', 'test623', 'testdb')
-    tbl = db.create('sample', name='VARCHAR(20)', date='DATE', test='VARCHAR(50)')
+    tbl = db.create('sample', ('name', 'VARCHAR(20)'),
+                    ('date', 'DATE'), ('test', 'VARCHAR(50)'))
+
+    for f, t, n, k, d, e in db.describe(tbl):
+        print "Field: %s Takes in %s" % (f, t)
+
+    db.insert(tbl, "Shotaro Ikeda", "1995-12-02", "my name")
+    db.insert(tbl, "Professor", "0000-00-00", "mystery")
+    db.insert(tbl, "Some random guy", "2015-02-23", "NULL")
+    # db.insert(tbl, "A")
+
+    getdb = db.retrieve(tbl)
+
+    return getdb
+
+if __name__ == '__main__':
+    testing = testcase()
+    print testing
+    for n, d, t in testing:
+        print "%25s    %10s     %10s" % (n, d, t)
+        
--- backend/bin/module/opendatabase.py
+++ backend/bin/module/opendatabase.py
@@ -1,5 +1,6 @@
 import MySQLdb as mdb
 
+
 class ManageTable(object):
     """Creates a test table, only to be used for demo purposes"""
 
@@ -7,7 +8,7 @@ class ManageTable(object):
         """Initialize which user to login as"""
         self.con = mdb.connect(net_loc, user, password, db)
 
-    def create(self, tbl_name, **kwargs):
+    def create(self, tbl_name, *args):
         """Initialize a table with certain elements"""
         with self.con:
 
@@ -16,30 +17,44 @@ class ManageTable(object):
             cur.execute(command)
 
             tbl_args = ""
-            for key, value in kwargs.items():
+            for key, value in args:
                 tbl_args += " %s %s," % (key, value)
-            
-            tbl_args = tbl_args.rstrip(",") # remove the last comma
+
+            tbl_args = tbl_args.rstrip(",")   # remove the last comma
 
             command = "CREATE TABLE %s(%s)" % (tbl_name, tbl_args)
             cur.execute(command)
         return tbl_name
 
-    def insert(self, tbl, var, *val):
-        """Insert data into a table of choice"""
+    def describe(self, tbl):
+        """mimics the describe function that mysql provides"""
         with self.con:
             cur = self.con.cursor()
-            for elements in val:
-                command = "INSERT INTO %s(%s) VALUES(%s)" % (tbl, var, elements)
-                cur.execute(command)
+            cur.execute("DESCRIBE %s" % (tbl))
+            dataset = cur.fetchall()
+            return dataset
+
+    def insert(self, tbl, *val):
+        """Insert a row of data into a table of choice"""
+        with self.con:
+            cur = self.con.cursor()
+            values = ""
+            for element in val:
+                if element == 'NULL':
+                    values += "NULL,"
+                else:
+                    values += "\'%s\'," % (element)
+            values = values.rstrip(",")
+            command = "INSERT INTO %s VALUES(%s)" % (
+                tbl, values)
+            cur.execute(command)
 
     def retrieve(self, tbl):
-        """Obtains data from database as a dictionary format"""
+        """Obtains data from database as a Tuple"""
         with self.con:
-            cur = self.con.cursor(mdb.cursors.DictCursor)
+            cur = self.con.cursor()
 
             cur.execute('SELECT * FROM %s' % (tbl))
-            datadict = cur.fetchall()
-            
-            return datadict
-            
+            datatpl = cur.fetchall()
+
+            return datatpl
