63a2363ba6a1d0d57447df529b6ac6a7885119be
shotaroikeda <ikeda.shot@gmail.com>
2015-04-18 16:07:10 -0500

    first commit

---
+++ backend/app/module/sendmail.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+class InValidEmailError(Exception):
+    pass
+
+
+def send_mail(message, subject, *email):
+    if not email:
+        raise InValidEmailError('No email passed')
+    if not message:
+        raise InValidEmailError('No message passed')
+
+    for mail in email:
+        ech = subprocess.Popen(['echo', message], stdout=subprocess.PIPE)
+        output = subprocess.check_output(['mail', '-s', subject, mail],
+                                         stdin=ech.stdout)
+        ech.wait()
+        print output
