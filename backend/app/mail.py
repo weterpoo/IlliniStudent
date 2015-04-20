4478b1ee43a615e68002731981bb7cd664451b5b
shotaroikeda <ikeda.shot@gmail.com>
2015-04-19 21:57:50 -0500

    finished user authentication script

--- backend/app/mail.py
+++ backend/app/mail.py
@@ -1,11 +1,23 @@
 from module.sendmail import send_mail as sm
 from passlib.apps import custom_app_context as pwd_context
 from module.opendatabase import ManageTable as mt
+import login
 import random
 import time
 
 
 def send_confirm(emailin, userin, passin, netidin, majorin, gradin):
+    access = mt('localhost', 'authorized', 'aCep0ted0dd', 'studentdb')
+
+    cond = "username = \'%s\' or useremail = \'%s\' or usernetid = \'%s\'" % (un,
+                                                                              ue,
+                                                                              uid)
+    result = access.find("userinfo", ('username', 'useremail', 'usernetid'), cond)
+
+    if not (len(result) == 0):
+        # return something when it already exists
+        return "Error -1: user exists. Please contact for help."
+
     salt = generate_salt(random.randint(7, 10))
     salt += userin + passin + netidin
     salt = pwd_context.encrypt(salt)
@@ -37,6 +49,36 @@ def store_temp(emailin, userin, passin, netidin, majorin, gradin, salt):
     # Store information in the temp
     access.insert("temp", userin, emailin, passin, netidin,
                   majorin, gradin, salt, now)
+
+
+def check_id(idin):
+    """
+    Checks for the id then creates the user.
+    """
+    access = mt('localhost', 'authorized', 'aCep0ted0dd', 'studentdb')
+    cond = "confirmid = %s" % (idin)
+    userinfo = access.findall("temp",
+                   ("username", "useremail",
+                    "userpass", "usernetid",
+                    "usermajor", "usergrad"), cond)
+    if len(userinfo) == 0:
+        return "Error -1: No user found"
+
+    user = login.create_login(userinfo[0], userinfo[1], userinfo[2],
+                              userinfo[3], userinfo[4], userinfo[5])
+
+    send_thank_you(userinfo[1])
+
+    return user[1]
+
+
+def send_thank_you(email):
+    subject = "Welcome to IlliniStudent!"
+    body = "You have succesfully registered for IlliniStudent!\n"
+    body += "We will be continuously adding features, "
+    body += "and we hope you look forward to them!\n"
+
+    sm(subject, body, email)
 
 
 def send_email_to(email):
