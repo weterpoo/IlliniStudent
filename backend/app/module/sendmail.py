ffae1b908b31802eef9bf292a09c77082a09e213
Shotaro Ikeda <ikeda.shot@gmail.com>
2015-04-23 18:39:17 -0500

    mail formatting fixes again

--- backend/app/module/sendmail.py
+++ backend/app/module/sendmail.py
@@ -12,12 +12,12 @@ def send_mail(message, subject, *email):
     if not message:
         raise InValidEmailError('No message passed')
 
-    subject = "\"%s\\nContent-Type: text/html\"" % (subject)
+    subject = "\"%s\nContent-Type: text/html\"" % (subject)
     for mail in email:
-        cmd = "echo \"%s\"" % (message)
+        cmd = "echo \'%s\'" % (message)
         cmd += " | "
         cmd += "mail -s "
-        cmd += "\"$(echo -e %s)\" " % (subject)
+        cmd += "\"$(echo %s)\" " % (subject)
         cmd += "%s" % (mail)
         # Sadly due to os.system, we lose error checking....
         print subject
