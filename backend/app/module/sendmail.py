8a13f08ca08a01e1d8df8430f0386f7e61178374
shotaroikeda <ikeda.shot@gmail.com>
2015-04-23 13:06:49 -0500

    mail patch

--- backend/app/module/sendmail.py
+++ backend/app/module/sendmail.py
@@ -12,12 +12,12 @@ def send_mail(message, subject, *email):
     if not message:
         raise InValidEmailError('No message passed')
 
-    subject = "\"%s\nContent-Type: text/html\"\"" % (subject)
+    subject = "\"%s\nContent-Type: text/html\"" % (subject)
     for mail in email:
         cmd = "echo \"%s\"" % (message)
         cmd += " | "
         cmd += "mail -s "
-        cmd += "\"$(echo -e \"%s\nContent-Type: text/html\")\" "
+        cmd += "\"$(echo -e %s)\" " % (subject)
         cmd += "%s" % (mail)
         # Sadly due to os.system, we lose error checking....
         os.system(cmd)
